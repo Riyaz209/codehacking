309ccd3a5dd57b108f9c2f9c7441749f32771265
Riyaz <iamriyaz100@gmail.com>
2020-04-07 17:08:52 +0100

    Admin master page setup.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin');
+
+@section('content')
+    <h1>This is admni index page.</h1>
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,36 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+/*
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');*/
+
+/*
+mix.styles([
+    'public/css/vendor/normalize.css',
+    'public/css/vendor/videojs.css'
+], 'public/css/all.css');
+
+mix.scripts([
+    'public/js/admin.js',
+    'public/js/dashboard.js'
+], 'public/js/all.js');*/
+
+//'resources/assets/css/libs/styles.css'
+
+mix.styles([
+    'resources/assets/css/libs/blog-post.css',
+	'resources/assets/css/libs/bootstrap.css',
+	'resources/assets/css/libs/font-awesome.css',
+	'resources/assets/css/libs/metisMenu.css',
+    'resources/assets/css/libs/sb-admin-2.css',
+	'resources/assets/css/libs/styles.css'
+], 'public/css/libs.css');
+
+mix.scripts([
+    'resources/assets/js/libs/jquery.js',
+	'resources/assets/js/libs/bootstrap.js',
+	'resources/assets/js/libs/metisMenu.js',
+	'resources/assets/js/libs/sb-admin-2.js',
+    'resources/assets/js/libs/scripts.js'
+], 'public/js/libs.js');
