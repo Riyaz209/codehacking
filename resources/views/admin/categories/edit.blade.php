1311d56d0c10783b3c6a05d9c6368db54f71e329
Riyaz <iamriyaz100@gmail.com>
2020-04-16 16:37:16 +0100

    Able to create categories

---
+++ resources/views/admin/categories/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('title', 'Edit Categories')
+
+@section('content')
+
+	
+
+	<h1>Categories</h1>
+	
+	<div class="col-sm-6">
+	
+		{!! Form::model($category, ['method' => 'PATCH', 'action' => ['AdminCategoriesController@update', $category->id]]) !!}
+		
+			<div class="form-group">
+				{!! Form::label('name', 'Name:') !!}
+				{!! Form::text('name', null, ['class' => 'form-control']) !!}
+			</div>
+			
+							
+			<div class="form-group">
+				{!! Form::submit('Create Category', ['class' => 'btn btn-primary']) !!}
+			</div>
+		
+		{!! Form::close() !!}
+		
+	
+	</div>
+	
+	<div class="col-sm-6">
+	
+		
+	
+	</div>
+
+@endsection
